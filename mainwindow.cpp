7f1267786e96d535d149a4e1978de367dd9f5f8b
raxzers <gacova95@gmail.com>
2018-03-18 07:39:33 -0700

    el comienzo

---
+++ mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+void MainWindow::on_actionPlay_triggered()
+{
+
+}
+
+void MainWindow::on_actionPause_triggered()
+{
+
+}
+
+void MainWindow::on_actionStop_triggered()
+{
+
+}
